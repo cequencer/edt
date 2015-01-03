d30925725bc41f020e1401ed44331e725c078172
wanderer <mjbecze@gmail.com>
2015-01-02 21:41:04 -0500

    added code and storage exporting

--- dumpState.js
+++ dumpState.js
@@ -4,6 +4,7 @@ var Ethereum = require('ethereumjs-lib'),
   levelup = require('level'),
   through = require('through'),
   JSONStream = require('JSONStream'),
+  async = require('async'),
   parseArgs = require('minimist');
 
 var argv = parseArgs(process.argv.slice(2), {
@@ -20,6 +21,7 @@ if (argv.h) {
   var helpStr = '\
   --cpp           Reads the cpp clients db\n\
   --node          reads the node.js clients db\n\
+  --more          Show the stateRoots and codeHashes of accounts \n\
   --i <string>    The location of some state DB\n\
   --root <string> The state root in hex\n\
   ';
@@ -50,19 +52,54 @@ var Trie = Ethereum.Trie,
 state.root = new Buffer(argv.root, 'hex');
 
 var ts = through(function write(data) {
+  var self = this;
   var key = data.key.toString('hex');
   var value = utils.baToJSON(rlp.decode(data.value));
-  var parse = {
+  var parsed = {
     address: key,
     nonce: value[0],
     balance: value[1],
     stateRoot: value[2],
-    codeHash: value[3]
+    codeHash: value[3],
+    code: '',
+    storage: []
   };
 
-  // if(parse.codeHash !== utils.emptyHash.toString('hex') )
+  function loadCode(done) {
+    if (parsed.codeHash !== utils.SHA3_NULL) {
+      stateDB.get(new Buffer(parsed.codeHash, 'hex'), {encoding:'binary'},  function(err, code) {
+        parsed.code = code.toString('hex');
+        done();
+      })
+    } else {
+      done();
+    }
+  }
 
-  this.queue(parsed); //data *must* not be null
+  function loadState(done){
+    var storage = new Trie(stateDB);
+    storage.root = new Buffer(parsed.stateRoot,'hex');
+    var sstream = storage.createReadStream();
+
+    sstream.on('data', function(data){
+      parsed.storage.push({
+        key: data.key.toString('hex'),
+        value: data.value.toString('hex')
+      });
+    });
+
+    sstream.on('end', done);
+  }
+
+  async.parallel([loadCode, loadState], function(){
+    if (!argv.more) {
+      delete parsed.stateRoot;
+      delete parsed.codeHash;
+    }
+
+    self.queue(parsed); //data *must* not be null
+  });
+
 });
 
 stream.pipe(ts);
